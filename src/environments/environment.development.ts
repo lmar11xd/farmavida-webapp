f3359076d8bf59fafc8a1048366c1aacba945303
Luis Miguel Alvarado <lmar11xd@gmail.com>
2025-03-23 09:51:45 -0500

    Base del projecto funcional

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,11 @@
 export const environment = {
     production: false,
-    SUPABASE_URL: 'https://xwimyflvkbzduoflzemb.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3aW15Zmx2a2J6ZHVvZmx6ZW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMTYxOTIsImV4cCI6MjA1Nzg5MjE5Mn0.TemKWXbYmDi0W7nFpE4gDwOdpQnW_yqKjXj2fJDxpUU'
+    firebase: {
+        apiKey: "API_KEY",
+        authDomain: "PROJECT_NAME.firebaseapp.com",
+        projectId: "PROJECT_ID",
+        storageBucket: "STORAGE_BUCKET",
+        messagingSenderId: "MSG_SENDERID",
+        appId: "APP_ID"
+    }
 };
